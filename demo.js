c5bbed13e72def6caf466353c48b32dc99a2a737
NoaTal1996 <noaai@post.bgu.ac.il>
2021-03-21 15:34:36 +0200

    4

--- demo.js
+++ demo.js
@@ -10,13 +10,27 @@ const funcSel=()=>{
 
 
 const funSumbit=()=>{
-    clear();
-    alert("success!!");
+   clear();
 }
-
 const clear=()=>{
     document.getElementById("smile").value ="";
     document.getElementById("msgSmile").value ="";
-    document.getElementsByClassName("input1").value ="";
+    document.getElementById("fname").value ="";
+    document.getElementById("lname").value ="";
+    document.getElementById("mail").value ="";
+
+    
 }
 
+$(function() {
+    // Initializes and creates emoji set from sprite sheet
+    window.emojiPicker = new EmojiPicker({
+      emojiable_selector: '[data-emojiable=true]',
+      assetsPath: 'http://onesignal.github.io/emoji-picker/lib/img/',
+      popupButtonClasses: 'fa fa-smile-o'
+    });
+    // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
+    // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
+    // It can be called as many times as necessary; previously converted input fields will not be converted again
+    window.emojiPicker.discover();
+  });
