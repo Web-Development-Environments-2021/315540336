3421d335518fa30cc32635c48953d4f3dc3f8a69
NoaTal1996 <noaai@post.bgu.ac.il>
2021-03-24 19:43:24 +0200

    8

--- demo.js
+++ demo.js
@@ -1,19 +1,9 @@
-const funcSel=()=>{
-    let msg = document.getElementById("msgSmile");
-    let ele = document.getElementById("smile");
-    let s = ele.options[ele.selectedIndex].text;
-    console.log(s)
-    msg.value += s;
-
-    }
-
-
 
 const funSumbit=()=>{
+   setTimeout(100)
    clear();
 }
 const clear=()=>{
-    document.getElementById("Subject").value ="";
     document.getElementById("msgSmile").value ="";
     document.getElementById("fname").value ="";
     document.getElementById("lname").value ="";
@@ -21,4 +11,6 @@ const clear=()=>{
 
     
 }
+
+
 
